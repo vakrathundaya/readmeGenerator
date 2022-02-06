70ee8157901759ced95b52b449d7ddaa6f0a1642
Radhika <cu9rad@gmail.com>
2022-02-05 23:24:19 -0500

    handling error functionality added

--- index.js
+++ index.js
@@ -36,6 +36,35 @@ const getData = () => {
             message: 'How do you use this app?'
         },
 
+        {
+            type: 'input',
+            name: 'install',
+            message: 'What are the steps required to install your project?'
+        },
+
+        {
+            type: 'input',
+            name: 'test',
+            message: 'What command should be run to run tests?'
+        },
+
+        {
+            type: 'input',
+            name: 'contributors',
+            message: 'what does the user need to know about contributing to the repo?'
+
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'What is your GitHub username?'
+
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your email address?'
+        }
     ]);
 };
 
@@ -43,7 +72,13 @@ const getData = () => {
 // function to write README file using file system fs
 const writeFile = data => {
     fs.writeFile('README.md', data, err => {
-
+        // if there is an error 
+        if (err) {
+            console.log(err);
+            return;
+        } else {
+            console.log("Your README has been successfully created!")
+        }
 
     })
 }
