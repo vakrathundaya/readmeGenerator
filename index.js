ebbc0884bda96ce86bac4c2b60cdd994cb6c0e09
Radhika <cu9rad@gmail.com>
2022-02-05 23:06:10 -0500

    function initialization added

--- index.js
+++ index.js
@@ -2,20 +2,35 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 
+//readme.md
+const generateREADME = require('./template.js');
+
+
 //question
 const getData = () => {
     return inquirer.prompt([
-    
-    
-   
-]);
+
+
+
+    ]);
 };
 
 
-    // function to write README file using file system fs
-    const writeFile = data => {
-        fs.writeFile('README.md', data, err => {
+// function to write README file using file system fs
+const writeFile = data => {
+    fs.writeFile('README.md', data, err => {
 
 
-        })
-    }
+    })
+}
+
+//initialize program
+
+getData()
+    .then(data => {
+        return generateREADME(data);
+
+    })
+    .then(data => {
+        return writeFile(data);
+    })  
