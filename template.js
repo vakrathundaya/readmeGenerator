649a47a9ce85f6627fa3fa40771776c20986ca30
Radhika <cu9rad@gmail.com>
2022-02-05 23:12:17 -0500

    template file added

---
+++ template.js
@@ -0,0 +1,39 @@
+return `# ${data.title}
+
+
+    ## Description 
+    ${data.description}
+  
+
+    ## Table of Contents
+  
+     [Installation](#installation)
+     [Usage](#usage)
+     [License](#license)
+     [Contributing](#contributing)
+     [Tests](#tests)
+     [Questions](#questions)
+    
+  
+    ## Installation 
+    ${data.install}
+  
+    ## Usage 
+    ${data.usage}
+  
+    ## License 
+    This project is license under ${data.license}
+    ![Github licence](http://img.shields.io/badge/license-${data.license}-blue.svg)
+  
+    ## Contributing 
+    ${data.contributors}
+  
+  
+    ## Tests
+    ${data.test}
+  
+  
+    ## Questions
+    If you have any questions about this projects, please contact me directly at ${data.email}. You can view more of my projects at https://github.com/${data.github}.
+  `;
+  
