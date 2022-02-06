ee8983d847066c4bc643a7c2002d16dc49fd345d
Radhika <cu9rad@gmail.com>
2022-02-05 23:02:25 -0500

    function added

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 //required modules
 const fs = require('fs');
 const inquirer = require('inquirer');
+
+//question
+const getData = () => {
+    return inquirer.prompt([
+    
+    
+   
+]);
+};
+
+
+    // function to write README file using file system fs
+    const writeFile = data => {
+        fs.writeFile('README.md', data, err => {
+
+
+        })
+    }
